5545e67dc566f5ff346e0d9c2a1863a4d317dfb4
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2025-08-13 11:47:40 +0330

    Initial Commit

---
+++ tasketoTests/tasketoTests.swift
@@ -0,0 +1,17 @@
+//
+//  tasketoTests.swift
+//  tasketoTests
+//
+//  Created by Mohammadsadra on 8/13/25.
+//
+
+import Testing
+@testable import tasketo
+
+struct tasketoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
